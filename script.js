faafb8097ec3781493a484722e326ddc60851b6a
Miruna Craciun <157888228+mimi-mc@users.noreply.github.com>
2024-02-08 18:50:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+let winnerBtn = Math.floor(Math.random() * 3) + 1;
 
+function checkButton() {
+    let clickedBtn = parseInt(document.activeElement.innerHTML);
+    if (clickedBtn === winnerBtn) {
+        alert(`Winning button! Congratulations!!`);
+    } else {
+        alert(`Losing button :( Try again.`);
+    }
+}
