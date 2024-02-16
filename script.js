fecefabc37d24a52765a2d8cb1eac8aa8d6ca859
Miruna Craciun <157888228+mimi-mc@users.noreply.github.com>
2024-02-16 17:02:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function getWinningButton() {
-	let noButtons = document.getElementById("noButtons").value;
-	let winningBtn = Math.floor(Math.random() * noButtons) + 1;
-	return winningBtn;
+    let noButtons = document.getElementById("noButtons").value;
+    let winningBtn = Math.floor(Math.random() * noButtons) + 1;
+    return winningBtn;
 }
 
-checkButton = function() {
-	let winningBtn = getWinningButton();
-	if (this.Id === winningBtn) {
-		alert(`Winning button! Congratulations!!`);
-	} else {
-		alert(`Losing button :( Try again.`);
-	}
+checkButton = function () {
+    let winningBtn = getWinningButton();
+    if (this.Id === winningBtn) {
+        alert(`Winning button! Congratulations!!`);
+    } else {
+        alert(`Losing button :( Try again.`);
+    }
 }
 
 function generateButtons() {
-	let winningBtn = getWinningButton();
-	let noButtons = document.getElementById("noButtons").value;
-	for (let i = 1; i <= noButtons; ++i) {
-		let button = document.createElement("button");
-		button.className = "btn btn-info";
-		button.Id = i;
-		button.textContent = "Button " + i;
-		button.addEventListener("click", checkButton);
-		generateButtonsDiv.appendChild(button);
-	}
+    let winningBtn = getWinningButton();
+    let noButtons = document.getElementById("noButtons").value;
+    for (let i = 1; i <= noButtons; ++i) {
+        let button = document.createElement("button");
+        button.className = "btn btn-info";
+        button.Id = i;
+        button.textContent = "Button " + i;
+        button.addEventListener("click", checkButton);
+        generateButtonsDiv.appendChild(button);
+    }
 }
